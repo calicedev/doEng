83063561fa0cacf80592154365024dd9a4051345
임혜은 <calicedev@gmail.com>
2023-03-06 16:30:19 +0900

    [BE]feat:member Entity생성(S08P22A601-44)

---
+++ backend/doeng/src/main/java/com/ssafy/doeng/data/entity/member/Member.java
@@ -0,0 +1,38 @@
+package com.ssafy.doeng.data.entity.member;
+
+import com.ssafy.doeng.data.entity.BaseEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+public class Member extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false)
+    private String member_id;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String nickname;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String phone;
+
+}
