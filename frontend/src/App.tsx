df678c2198bc4e265b4b274ec2d13c2d51f37b13
leeyuhmin <yuhmin92@naver.com>
2023-03-08 17:46:13 +0900

    [FE] feat: progresslist component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
-import React from "react"
-import { Route } from "react-router"
-import { Routes } from "react-router-dom"
-import "./App.css"
-import Login from "./components/SignComponents/Login/Login"
-import Signup from "./components/SignComponents/Signup/Signup"
-import HomePage from "./pages/HomePage"
-import SignPage from "./pages/SignPage"
+import React from "react";
+import { Route } from "react-router";
+import { Routes } from "react-router-dom";
+import "./App.css";
+import Login from "./components/SignComponents/Login/Login";
+import Signup from "./components/SignComponents/Signup/Signup";
+import HomePage from "./pages/HomePage";
+import ProgressListPage from "./pages/ProgressListPage";
+import SignPage from "./pages/SignPage";
 
 function App() {
   return (
@@ -16,9 +17,10 @@ function App() {
           <Route path={`login`} element={<Login />} />
           <Route path={`signup`} element={<Signup />} />
         </Route>
+        <Route path={"/progress"} element={<ProgressListPage />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/ProgressListPage.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import dummy from "../components/Progress/DummyData/ProgressList.json";
+import ProgressList from "../components/Progress/ProgressList/ProgressList";
+
+type Tale = {
+  id: number;
+  title: string;
+  backgroundImage: string;
+  progress: number;
+  correctWordCount: number;
+  totalWordCount: number;
+};
+
+function ProgressListPage() {
+  const { taleList }: { taleList: Tale[] } = dummy;
+
+  return (
+    <div>
+      <h1>ProgressListPage</h1>
+      <div className="grid grid-cols-5 gap-4">
+        {taleList.map((tale: Tale) => (
+          <ProgressList key={tale.id} tale={tale} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default ProgressListPage;
