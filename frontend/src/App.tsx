4c3fd2b0382b701fa9a450dd554a58daf40ebc3e
HanKyeon <hgh21233@gmail.com>
2023-03-12 12:58:08 +0900

    [FE] Feat: meta tag's og properties

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
-import { Route } from "react-router"
-import { Routes } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import "./App.css"
 import Login from "./components/SignComponents/Login"
 import Signup from "./components/SignComponents/Signup"
