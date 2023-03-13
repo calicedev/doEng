6289c06104cdae2a1115404ad8590b1ee930fa2f
leeyuhmin <yuhmin92@naver.com>
2023-03-13 15:03:41 +0900

    [FE] feat: progress detail page

--- frontend/src/components/MyPageComponents/ProgressDetail/ProgressDetailTest.tsx
+++ frontend/src/components/MyPageComponents/ProgressDetail/ProgressDetailTest.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react"
 
 interface Word {
   engWord: string
@@ -15,26 +15,74 @@ interface TaleProps {
 }
 
 function ProgressDetailTest({ taleTest }: TaleProps) {
+  const wordTrueCounts: number[] = taleTest.wordList.map(
+    (word: Word) => word.correctList.filter((value: boolean) => value).length,
+  )
+
+  const testTrueCounts: number[] = taleTest.wordList[0].correctList.map(
+    (_, i) =>
+      taleTest.wordList.reduce(
+        (acc, word) => acc + (word.correctList[i] ? 1 : 0),
+        0,
+      ),
+  )
+
+  const totalWords: number = taleTest.wordList.length
+  const totalTests: number = taleTest.testCount
   return (
     <div>
-      <table>
+      <table className="table-auto border-collapse border-spacing-20 border-black border-4">
         <thead>
           <tr>
             <th>Result</th>
             {[...Array(taleTest.testCount)].map((_, i) => (
-              <th key={i}>{i + 1}회</th>
+              <th
+                key={i}
+                className="border border-collapse border-black px-4 py-2"
+              >
+                {i + 1}회
+              </th>
             ))}
+            <th>정답률</th>
           </tr>
         </thead>
         <tbody>
           {taleTest.wordList.map((word) => (
             <tr key={word.engWord}>
-              <td>{word.engWord}</td>
+              <td className="border border-collapse border-black px-4 py-2">
+                {word.engWord}
+              </td>
               {word.correctList.map((correct, i) => (
-                <td key={i}>{correct.toString()}</td>
+                <td
+                  key={i}
+                  className="border border-collapse border-black px-4 py-2"
+                >
+                  {correct.toString()}
+                </td>
               ))}
+              <td className="border border-collapse border-black px-4 py-2">
+                {(
+                  (wordTrueCounts[taleTest.wordList.indexOf(word)] /
+                    totalTests) *
+                  100
+                ).toFixed(2)}
+                %
+              </td>
             </tr>
           ))}
+          <tr>
+            <td className="border border-collapse border-black px-4 py-2">
+              점수
+            </td>
+            {testTrueCounts.map((count, i) => (
+              <td
+                key={i}
+                className="border border-collapse border-black px-4 py-2"
+              >
+                {count}
+              </td>
+            ))}
+          </tr>
         </tbody>
       </table>
     </div>
