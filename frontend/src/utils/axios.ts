37d4fb77044006a7253af259546aa0c20715b7f5
HanKyeon <hgh21233@gmail.com>
2023-03-16 18:00:16 +0900

    [FE] Feat: interceptor 수정

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,4 +1,4 @@
-import axios from "axios"
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios"
 import store, { DispatchToast } from "store/index"
 import { tokenActions } from "store/tokenSlice"
 
@@ -11,7 +11,7 @@ const apiRequest = axios.create({
   // baseURL: "https://j8a601.p.ssafy.io", // 서버 주소
   baseURL: "http://70.12.246.176:8200", // 서버 주소
   withCredentials: true, // 쿠키 사용을 위해 설정
-  timeout: 3000,
+  timeout: 10000,
 })
 
 // request 인터셉터
@@ -19,13 +19,13 @@ apiRequest.interceptors.request.use(
   (config) => {
     const state = store.getState() // 리덕스 상태 가져오기
     const accessToken = state.token.accessToken // 리덕스 accessToken 읽기
-    const refreshToken = state.token.refreshToken
+    // const refreshToken = state.token.refreshToken
     if (accessToken) {
-      config.headers.common[`accessToken`] = `Bearer ${accessToken}` // 리덕스에 accessToken이 있을 경우 Authorization 헤더 추가
+      config.headers.Authorization = `Bearer ${accessToken}` // 리덕스에 accessToken이 있을 경우 Authorization 헤더 추가
     }
-    if (refreshToken) {
-      config.headers.common[`refreshToken`] = `Bearer ${refreshToken}`
-    }
+    // if (refreshToken) {
+    //   config.headers.common[`refreshtoken`] = `Bearer ${refreshToken}`
+    // }
     return config
   },
   (error) => {
@@ -35,42 +35,72 @@ apiRequest.interceptors.request.use(
 
 // response 인터셉터
 apiRequest.interceptors.response.use(
-  (response) => {
-    console.log("response", response)
-    console.log("accessToken: ", response.headers[`accessToken`])
-    console.log("refreshToken: ", response.headers[`refreshToken`])
-    return response
+  (res: AxiosResponse) => {
+    const newAccessToken = res.headers["accesstoken"] || res.data.accesstoken
+    const newRefreshToken = res.headers["refreshtoken"] || res.data.refreshtoken
+    if (newAccessToken) {
+      store.dispatch(
+        tokenActions.setAccessToken({ accessToken: newAccessToken }),
+      ) // 리덕스 accessToken 갱신
+    }
+    if (newRefreshToken) {
+      store.dispatch(
+        tokenActions.setRefreshToken({ refreshToken: newRefreshToken }),
+      )
+    }
+    console.log("response", res)
+    return res
   },
   async (error) => {
-    console.log("error", error)
+    console.log(`errorStatusCode: ${error.response.status}`)
+    console.log(error)
+
+    const state = store.getState() // 리덕스 상태 가져오기
+    const accessToken = state.token.accessToken // 리덕스 accessToken 읽기
+    const refreshToken = state.token.refreshToken
 
     const originalConfig = error.config // 기존 요청 정보 저장 (accessToken 요청 후 재발급을 위해)
     const response = error.response // 에러 정보
 
     if (response.status === 401) {
       // access Token 재발급
-      const config = {} // accessToken 재발급 관련 설정
-      await axios
-        .request(config)
+      const config: AxiosRequestConfig = {
+        method: `post`,
+        // baseURL: "https://j8a601.p.ssafy.io", // 서버 주소
+        baseURL: "http://70.12.246.176:8200", // 서버 주소
+        url: `/api/member/reissue`,
+        headers: {
+          accesstoken: accessToken,
+          refreshtoken: refreshToken,
+        },
+        data: {
+          accesstoken: accessToken,
+          refreshtoken: refreshToken,
+        },
+      } // accessToken 재발급 관련 설정
+      await axios(config)
         .then((res) => {
-          const newAccessToken = res.headers["accessToken"]
-          const newRefreshToken = res.headers["refreshToken"]
-          store.dispatch(
-            tokenActions.setAccessToken({ accessToken: newAccessToken }),
-          ) // 리덕스 accessToken 갱신
-          store.dispatch(
-            tokenActions.setRefreshToken({ refreshToken: newRefreshToken }),
-          )
-          originalConfig.headers.common[
-            "accessToken"
-          ] = `Bearer ${newAccessToken}`
-          originalConfig.headers.common[
-            "refreshToken"
-          ] = `Bearer ${newRefreshToken}`
-          // originalConfig.headers.Authorization = `Bearer ${newAccessToken}`
+          const newAccessToken =
+            res.headers["accesstoken"] || res.data.accesstoken
+          const newRefreshToken =
+            res.headers["refreshtoken"] || res.data.refreshtoken
+          if (newAccessToken) {
+            store.dispatch(
+              tokenActions.setAccessToken({ accessToken: newAccessToken }),
+            ) // 리덕스 accessToken 갱신
+            originalConfig.headers.common["accesstoken"] = `${newAccessToken}`
+            originalConfig.headers.Authorization = `Bearer ${newAccessToken}`
+          }
+          if (newRefreshToken) {
+            store.dispatch(
+              tokenActions.setRefreshToken({ refreshToken: newRefreshToken }),
+            )
+            originalConfig.headers.common["refreshtoken"] = `${newRefreshToken}`
+          }
           return apiRequest(originalConfig) // 기존 요청 새로운 token으로 재시도
         })
         .catch((err) => {
+          console.log("토큰 재발급 에러 : ", err)
           store.dispatch(DispatchToast("다시 로그인 해주세요.", false))
           // alert("다시 로그인 해주세요.")
         })
