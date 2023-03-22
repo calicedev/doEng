131b3199d1b7f046e3669a6f3e38bc8dbb964fa5
HanKyeon <hgh21233@gmail.com>
2023-03-22 17:59:17 +0900

    [FE] Feat: 그림 컴포넌트 제작 틀

---
+++ frontend/src/components/CanvasComponents/Drawing/Drawing.tsx
@@ -0,0 +1,25 @@
+export const a: number = 1
+import { FC, memo } from "react"
+import { useCanvas } from "../../../hooks/useCanvas"
+// import { SecondWaveBackground } from "./SecondWaveBackground"
+
+interface waveProps {
+  canvasWidth: number
+  canvasHeight: number
+}
+
+const Wave: FC<waveProps> = function ({ canvasWidth, canvasHeight }) {
+  const fillBackground = function (ctx: CanvasRenderingContext2D): void {
+    ctx.fillStyle = "rgba(249, 170, 70, 1)"
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight)
+  }
+  const animate = function (ctx: CanvasRenderingContext2D): void {
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+    fillBackground(ctx)
+    // wave1.drawWaveWithNoise(ctx)
+  }
+  const canvasRef = useCanvas(canvasWidth, canvasHeight, animate)
+  return <canvas ref={canvasRef} />
+}
+
+export default memo(Wave)
