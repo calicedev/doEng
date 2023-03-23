1b22ba00173f6d7bf4ab98e5c8fd0f10716413f5
HanKyeon <hgh21233@gmail.com>
2023-03-23 17:28:44 +0900

    [FE] Feat: 세션 만료 안내 및 로그인으로 이동 시키기.

--- frontend/src/components/UI/ProtectedRoute.tsx
+++ frontend/src/components/UI/ProtectedRoute.tsx
@@ -1,14 +1,29 @@
 import { useUserQuery } from "hooks/queries/user"
-import { useStoreSelector } from "hooks/useStoreSelector"
+import { useStoreDispatch, useStoreSelector } from "hooks/useStoreSelector"
 import SignPage from "pages/SignPage"
 import { PropsWithChildren, useEffect } from "react"
 import { Navigate, Outlet, Route } from "react-router-dom"
+import { DispatchToast } from "store"
 import { SpinnerDots } from "./Spinner"
 
 interface Props {}
 
 const ProtectedRoute = function ({ children }: PropsWithChildren<Props>) {
   const { isLoading, data } = useUserQuery()
+  const dispatch = useStoreDispatch()
+  useEffect(
+    function () {
+      if (isLoading) {
+        return
+      }
+      if (!data?.id) {
+        dispatch(
+          DispatchToast("세션이 만료되었습니다. 다시 로그인 해주세요.", false),
+        )
+      }
+    },
+    [isLoading, data?.id],
+  )
   return isLoading ? (
     <SpinnerDots />
   ) : data?.id ? (
