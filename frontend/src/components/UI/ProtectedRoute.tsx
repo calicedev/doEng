42e3f5df82416c343295779aafbd848da41893e7
HanKyeon <hgh21233@gmail.com>
2023-03-23 16:53:41 +0900

    [FE] Feat: KakaoPaymentBridge, ProtectedRoute by ReactQuery

--- frontend/src/components/UI/ProtectedRoute.tsx
+++ frontend/src/components/UI/ProtectedRoute.tsx
@@ -1,15 +1,21 @@
 import { useUserQuery } from "hooks/queries/user"
 import { useStoreSelector } from "hooks/useStoreSelector"
 import SignPage from "pages/SignPage"
-import { PropsWithChildren } from "react"
+import { PropsWithChildren, useEffect } from "react"
 import { Navigate, Outlet, Route } from "react-router-dom"
+import { SpinnerDots } from "./Spinner"
 
 interface Props {}
 
 const ProtectedRoute = function ({ children }: PropsWithChildren<Props>) {
-  const { data } = useUserQuery()
-
-  return !data?.id ? <Outlet /> : <Navigate to={`/member/login`} />
+  const { isLoading, data } = useUserQuery()
+  return isLoading ? (
+    <SpinnerDots />
+  ) : data?.id ? (
+    <Outlet />
+  ) : (
+    <Navigate to={`/member/login`} />
+  )
 }
 
 export default ProtectedRoute
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -3,6 +3,7 @@ import Wave from "../components/CanvasComponents/Wave/Wave"
 import { useWidthHeight } from "../hooks/useWidthHwight"
 import { useStoreDispatch } from "hooks/useStoreSelector"
 import { useNavigate } from "react-router-dom"
+import LoadingComp from "components/UI/LoadingComp"
 
 function HomePage() {
   const navigate = useNavigate()
@@ -10,24 +11,25 @@ function HomePage() {
   const canvasDivRef = useRef<HTMLDivElement>(null)
   const { width: canvasWidth, height: canvasHeight } =
     useWidthHeight(canvasDivRef)
-  useEffect(
-    function () {
-      navigate("/member/login")
-    },
-    [navigate],
-  )
+  // useEffect(
+  //   function () {
+  //     navigate("/member/login")
+  //   },
+  //   [navigate],
+  // )
   return (
-    <>
-      <div
+    <div className="h-full w-full box-border">
+      <LoadingComp />
+      {/* <div
         ref={canvasDivRef}
         className={`w-full h-full blur-sm canvas-under-bg-container`}
       >
         <Wave canvasWidth={canvasWidth} canvasHeight={canvasHeight} />
+      </div> */}
+      <div className={`h-full w-full flex items-center justify-center`}>
+        <div>홈인데요 홈인데요</div>
       </div>
-      <div className={``}>
-        <div>홈인데요</div>
-      </div>
-    </>
+    </div>
   )
 }
 
