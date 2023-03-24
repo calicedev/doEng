2dee67a87c5c28ed2baa7ef2814012fb1b0d2e56
wonyoung <isabella23@naver.com>
2023-03-24 17:45:55 +0900

    [BE]feat: 결제 저장 api 구현

---
+++ backend/doeng/src/main/java/com/ssafy/doeng/data/dto/tale/request/RequestTalePaymentDto.java
@@ -0,0 +1,22 @@
+package com.ssafy.doeng.data.dto.tale.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class RequestTalePaymentDto {
+    private long memberId;
+    private long taleId;
+
+    public void setMemberId(long memberId) {
+        this.memberId = memberId;
+    }
+}
--- backend/doeng/src/main/java/com/ssafy/doeng/service/tale/impl/TaleServiceImpl.java
+++ backend/doeng/src/main/java/com/ssafy/doeng/service/tale/impl/TaleServiceImpl.java
@@ -6,6 +6,7 @@ import com.ssafy.doeng.data.dto.review.response.ResponseReviewDto;
 import com.ssafy.doeng.data.dto.review.vo.ReviewSum;
 import com.ssafy.doeng.data.dto.scene.response.ResponseProgressSceneDto;
 import com.ssafy.doeng.data.dto.tale.request.RequestTaleDetailDto;
+import com.ssafy.doeng.data.dto.tale.request.RequestTalePaymentDto;
 import com.ssafy.doeng.data.dto.tale.response.ResponseMainTaleDetailDto;
 import com.ssafy.doeng.data.dto.tale.response.ResponseMainTaleDto;
 import com.ssafy.doeng.data.dto.tale.response.ResponsePaymentTaleDetailDto;
@@ -352,5 +353,19 @@ public class TaleServiceImpl implements TaleService {
             score = Math.round((sum / reviewSum.get().getCount()) * 10) / 10.0;
         }
         return score;
+    }
+
+    public void postTalePayment(RequestTalePaymentDto requestDto) {
+        LOGGER.info("[TaleServiceImpl] 결제 저장 service 들어옴");
+        Member member = common.getMember(requestDto.getMemberId());
+        Tale tale = common.getTale(requestDto.getTaleId());
+
+        Payment payment = Payment.builder()
+                .tale(tale)
+                .member(member)
+                .build();
+
+        paymentRepository.save(payment);
+        LOGGER.info("[TaleServiceImpl] 결제 저장 완료");
     }
 }
