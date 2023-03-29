763d215f0d4efbd98a1b1bdbd58b5b9e349f7852
HanKyeon <hgh21233@gmail.com>
2023-03-29 22:32:48 +0900

    [FE] Feat: useTransition hook 추가

--- frontend/src/hooks/useAnimate.ts
+++ frontend/src/hooks/useAnimate.ts
@@ -2,7 +2,7 @@ import React, { FC, PropsWithChildren } from "react"
 import { useEffect, useState, useCallback } from "react"
 
 interface animationHook {
-  (isOpened: boolean, appearClassName?: string, disappearClassName?: string): {
+  (isOpened?: boolean, appearClassName?: string, disappearClassName?: string): {
     isRender: boolean
     animationClasses: string
     animationEndHandler: () => void
@@ -10,31 +10,31 @@ interface animationHook {
 }
 
 export const useAnimate: animationHook = function (
-  opened,
+  isOpened = true,
   appearClassName = "animate-appear-from-bottom-fast",
   disappearClassName = "animate-disappear-to-bottom-fast",
 ) {
-  const [isAnimate, setIsAnimate] = useState<boolean>(opened)
-  const [isRender, setIsRender] = useState<boolean>(opened || isAnimate)
+  const [isAnimate, setIsAnimate] = useState<boolean>(isOpened)
+  const [isRender, setIsRender] = useState<boolean>(isOpened || isAnimate)
   const [animationClasses, setAnimationClasses] = useState<string>("")
 
   useEffect(
     function () {
-      if (opened) {
+      if (isOpened) {
         setIsAnimate(() => true)
         setAnimationClasses(() => appearClassName + ` `)
       } else {
         setAnimationClasses(() => disappearClassName + ` absolute`)
       }
     },
-    [opened, appearClassName, disappearClassName],
+    [isOpened, appearClassName, disappearClassName],
   )
 
   useEffect(
     function () {
-      setIsRender(() => opened || isAnimate)
+      setIsRender(() => isOpened || isAnimate)
     },
-    [opened, isAnimate],
+    [isOpened, isAnimate],
   )
 
   useEffect(
@@ -49,16 +49,80 @@ export const useAnimate: animationHook = function (
   // 열지 않기 원하는데 animation이 끝났을 때만 false로 바꿔준다.
   const animationEndHandler: () => void = useCallback(
     function () {
-      if (opened === false) {
+      if (isOpened === false) {
         setIsAnimate(() => false)
       }
     },
-    [opened],
+    [isOpened],
   )
 
   return {
     isRender,
     animationClasses,
     animationEndHandler,
+  }
+}
+
+interface transitionHook {
+  (
+    isOpen?: boolean,
+    appearTransitionClass?: string,
+    disappearTransitionClass?: string,
+  ): {
+    isRender: boolean
+    transitionClasses: string
+    transitionEndHandler: () => void
+  }
+}
+export const useTransition: transitionHook = function (
+  isOpened = true,
+  appearTransitionClass = "animate-appear-from-bottom-fast",
+  disappearTransitionClass = "animate-disappear-to-bottom-fast",
+) {
+  const [isTransition, setIsTransition] = useState<boolean>(isOpened)
+  const [isRender, setIsRender] = useState<boolean>(isOpened || isTransition)
+  const [transitionClasses, setTransitionClasses] = useState<string>("")
+
+  useEffect(
+    function () {
+      if (isOpened) {
+        setIsTransition(() => true)
+        setTransitionClasses(() => appearTransitionClass + ` `)
+      } else {
+        setTransitionClasses(() => disappearTransitionClass + ` absolute`)
+      }
+    },
+    [isOpened, appearTransitionClass, disappearTransitionClass],
+  )
+
+  useEffect(
+    function () {
+      setIsRender(() => isOpened || isTransition)
+    },
+    [isOpened, isTransition],
+  )
+
+  useEffect(
+    function () {
+      if (!isRender) {
+        setTransitionClasses(() => "")
+      }
+    },
+    [isRender],
+  )
+
+  // 열지 않기 원하는데 transition 끝났을 때만 false로 바꿔준다.
+  const transitionEndHandler: () => void = useCallback(
+    function () {
+      if (isOpened === false) {
+        setIsTransition(() => false)
+      }
+    },
+    [isOpened],
+  )
+  return {
+    isRender,
+    transitionClasses,
+    transitionEndHandler,
   }
 }
