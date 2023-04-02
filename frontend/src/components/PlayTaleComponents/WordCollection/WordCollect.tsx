8e0a0bd43c0393826fa1add78cb81298d70be8a1
leeymk <leeymk11@gmail.com>
2023-04-02 22:48:01 +0900

    [FE] feat: word-collect pagination

--- frontend/src/components/PlayTaleComponents/WordCollection/WordCollect.tsx
+++ frontend/src/components/PlayTaleComponents/WordCollection/WordCollect.tsx
@@ -8,8 +8,6 @@ import { useState } from "react"
 function WordCollect() {
   const [currentPage, setCurrentPage] = useState(1)
   const [itemsPerPage, setItemsPerPage] = useState(8)
-  const indexOfLastItem = currentPage * itemsPerPage
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage
 
   const {
     isLoading: WordListLoading,
@@ -17,24 +15,24 @@ function WordCollect() {
     data: WordList,
   } = useWordList()
 
-  const currentItems = WordList?.slice(indexOfFirstItem, indexOfLastItem)
-  console.log("11111333333")
-  console.log(WordList)
+  if (WordListLoading) {
+    return <LoadingPage />
+  }
+
+  if (WordListError || !WordList || WordList.length === 0) {
+    return <div>단어 카드가 존재하지 않습니다</div>
+  }
+
+  const indexOfLastItem = currentPage * itemsPerPage
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage
+
+  const currentItems = WordList.slice(indexOfFirstItem, indexOfLastItem)
+  const totalPages = Math.ceil(WordList.length / itemsPerPage)
+
+  console.log(WordList, "11123344")
 
   const handlePageChange = (pageNumber: number) => {
     setCurrentPage(pageNumber)
-  }
-
-  let totalPages: number = 0
-
-  if (WordList) {
-    const totalPages: number = Math.ceil(WordList.length / itemsPerPage)
-  }
-
-  if (WordListLoading) {
-    return <LoadingPage />
-  } else if (!WordList) {
-    return <div>단어 카드가 존재하지 않습니다</div>
   }
 
   return (
@@ -47,38 +45,25 @@ function WordCollect() {
         className="canvas-under-bg-container bg-vegis absolute -z-[60] h-full w-full"
       />
 
-      {WordList ? (
-        <div className="w-full h-full grid grid-rows-2 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 pl-[10%] pt-[10%] pr-[8%] pb-[10%]">
-          {currentItems?.map((word) => (
-            <WordCard key={`word-card-${word.id}`} word={word} />
-          ))}
+      <div className="w-full h-full grid grid-rows-2 grid-cols-2 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-4 gap-6 pl-[10%] pt-[10%] pr-[4%] pb-[10%]">
+        {currentItems.map((word) => (
+          <WordCard key={`word-card-${word.id}`} word={word} />
+        ))}
+      </div>
 
-          {/* {WordList?.map((word) => (
-            <WordCard key={`word-card-${word.id}`} word={word} />
-          ))} */}
-        </div>
-      ) : (
-        <div>잘못된 접근입니다.</div>
-      )}
-
-      {WordList ? (
-        <div className="flex justify-center mt-4">
-          {Array.from({ length: totalPages }, (_, i) => (
-            <button
-              key={i}
-              className={`mx-1 p-1 rounded ${
-                i + 1 === currentPage ? "bg-blue-500 text-white" : "bg-white"
-              }`}
-              onClick={() => handlePageChange(i + 1)}
-            >
-              {i + 1}
-            </button>
-          ))}
-        </div>
-      ) : (
-        <div>1</div>
-      )}
-      {/* </div> */}
+      <div className="flex justify-center mt-4">
+        {Array.from({ length: totalPages }, (_, i) => (
+          <button
+            key={i}
+            className={`mx-1 p-1 rounded ${
+              i + 1 === currentPage ? "bg-blue-500 text-white" : "bg-white"
+            }`}
+            onClick={() => handlePageChange(i + 1)}
+          >
+            {i + 1}
+          </button>
+        ))}
+      </div>
     </>
   )
 }
