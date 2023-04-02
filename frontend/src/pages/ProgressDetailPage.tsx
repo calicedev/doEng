6c509101862dd39c64c3336fbac2389ff86b966a
Byongho96 <unlike96@gmail.com>
2023-04-02 22:21:45 +0900

    [FE][feat] 마이페이지 CSS

--- frontend/src/pages/ProgressDetailPage.tsx
+++ frontend/src/pages/ProgressDetailPage.tsx
@@ -28,22 +28,27 @@ function ProgressDetailPage() {
       {progressDetailLoading ? (
         <CommonLoading />
       ) : progressDetail ? (
-        <div className="flex flex-col items-center sm:flex-row sm:items-stretch gap-10 p-6 overflow-y-auto">
-          <div className={`self-center min-w-[300px] w-[80%] sm:w-[30%]`}>
+        <div
+          id="progress-detail-container"
+          className="flex flex-col items-center sm:flex-row sm:items-stretch gap-10 h-full p-6 overflow-x-hidden overflow-y-auto"
+        >
+          <div
+            className={`self-center min-w-[250px] max-w-[400px] w-[80%] sm:w-[30%]`}
+          >
             <ProgressDetailHeader tale={progressDetail} />
           </div>
-          <div className="flex-1 flex flex-col gap-5">
-            <div className="w-full h-[50%]">
+          <div className="flex-1 flex flex-col gap-5 w-full">
+            <div className="flex-1 w-full">
               {progressDetail.sceneList && (
                 <ProgressDetailSceneList
                   key={progressDetail.id}
-                  photoList={progressDetail.sceneList}
+                  sceneList={progressDetail.sceneList}
                 />
               )}
             </div>
-            <div className={`w-full h-[50%] overflow-scroll`}>
+            <div className={`flex-1 w-full`}>
               {progressDetail.testResult && (
-                <ProgressDetailTest taleTest={progressDetail.testResult} />
+                <ProgressDetailTest testResult={progressDetail.testResult} />
               )}
             </div>
           </div>
