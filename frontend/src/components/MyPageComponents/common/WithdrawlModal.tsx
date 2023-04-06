470018110bfdda5233b26823c606f673313dc43a
HanKyeon <hgh21233@gmail.com>
2023-04-06 17:39:14 +0900

    [FE] Feat: withdraw mutation 직접 설정

--- frontend/src/components/MyPageComponents/common/WithdrawlModal.tsx
+++ frontend/src/components/MyPageComponents/common/WithdrawlModal.tsx
@@ -1,19 +1,17 @@
 import MyPageButton from "components/MyPageComponents/common/MyPageButton"
 import { useNavigate, useLocation } from "react-router-dom"
 import React, { useState, useMemo, PropsWithChildren } from "react"
-import useApi from "hooks/useApi"
-import { useUserMutation } from "hooks/queries/queries"
 import { useStoreDispatch } from "hooks/useStoreSelector"
 import { DispatchLogout } from "store"
 import { useQueryClient } from "@tanstack/react-query"
 import { queryKeys } from "hooks/queries/queryKeys"
+import apiRequest from "utils/axios"
 
 interface Props {
   closeModal: () => void
 }
 
 const WithdrawlModal = function ({ closeModal }: PropsWithChildren<Props>) {
-  const { mutateAsync: WithdrawMutate } = useUserMutation()
   const dispatch = useStoreDispatch()
   const navigate = useNavigate()
   const queryClient = useQueryClient()
@@ -21,7 +19,7 @@ const WithdrawlModal = function ({ closeModal }: PropsWithChildren<Props>) {
   // 탈퇴하기
 
   const withdrawHandler = function () {
-    WithdrawMutate({
+    apiRequest({
       method: `delete`,
       url: `/api/member`,
     })
@@ -29,6 +27,7 @@ const WithdrawlModal = function ({ closeModal }: PropsWithChildren<Props>) {
         dispatch(DispatchLogout())
       })
       .then(() => {
+        queryClient.invalidateQueries(queryKeys.user())
         queryClient.removeQueries(queryKeys.user())
         navigate(`/`)
       })
